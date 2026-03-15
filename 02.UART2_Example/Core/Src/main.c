f0fea57dacb5aa61b1f0d0cc7e8dbff412962552
man Unaligned <reflow_soldering@naver.com>
2026-03-15 21:32:41 +0900

    UART ing

--- 02.UART2_Example/Core/Src/main.c
+++ 02.UART2_Example/Core/Src/main.c
@@ -8,9 +8,16 @@
 
 #include "stm32f4xx_hal.h"
 #include "main.h"
+#include <string.h>
 
-void SystemClockConfig(void)
+void SystemClockConfig(void);
+void UART2_Init(void);
+void Error_handler(void);
+uint8_t convert_to_capital(uint8_t data);
 
+UART_HandleTypeDef huart2;
+
+char *user_data = "The application is running\n\r";
 
 int main(void)
 {
@@ -18,6 +25,33 @@ int main(void)
 	HAL_Init();
 
 	SystemClockConfig();
+
+	UART2_Init();
+
+	uint16_t len_size = strlen(user_data);
+	HAL_UART_Transmit(&huart2, (uint8_t *)user_data, len_size, HAL_MAX_DELAY);
+
+	uint8_t rcvd_data;
+	uint8_t data_buffer[100];
+	uint32_t count = 0;
+	while(1)
+	{
+		HAL_UART_Receive(&huart2, &rcvd_data, 1, HAL_MAX_DELAY);
+		if(rcvd_data == '\r')
+		{
+			break;
+		}
+		else
+		{
+			// 수신을 그대로 출력
+			//data_buffer[count++]=rcvd_data;
+			data_buffer[count++] = convert_to_capital(rcvd_data);
+		}
+	}
+	data_buffer[count++] = '\r';
+
+	HAL_UART_Transmit(&huart2, data_buffer, count, HAL_MAX_DELAY);
+	while(1);
 
 	return 0;
 
@@ -27,4 +61,39 @@ void SystemClockConfig(void)
 {
 
 
+}
+
+void UART2_Init(void)
+{
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	if( HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		//문제가 발생하면 들어옴
+		Error_handler();
+	}
+
+}
+
+void Error_handler(void)
+{
+	while(1);
+}
+
+uint8_t convert_to_capital(uint8_t data)
+{
+	if(data >= 'a' && data<= 'z')
+	{
+		data = data - ('a' - 'A');
+	}
+	else
+	{
+		data = data;
+	}
+	return data;
 }
--- 02.UART2_Example/Core/Src/msp.c
+++ 02.UART2_Example/Core/Src/msp.c
@@ -17,8 +17,40 @@ void HAL_MspInit(void)
 	// 1. 프로세서 우선순위 그룹화
 	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 	// 2. 익셉션 인에이블
+	SCB->SHCSR |= (0x7 << 16); //Usabe Fault, Memory Fault, Bus Fault System Exception 활성화
 
 	// 3. 시스템 익셉션 우선순위 규정
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+	//Systick은 HAL_Init에 있음
+}
 
-	//
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+	//저수준 초기화 진행. USART2 장치
+
+	//2-1. UART를 위한 GPIO 변수 생성
+	GPIO_InitTypeDef gpio_uart;
+
+	//1. USART2 이용 위한 클럭 활성화 UART 장치와 GPIOA 활성화 필수임
+	__HAL_RCC_USART2_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	//2. 핀 MUX 하여 용도 설정
+	gpio_uart.Pin = GPIO_PIN_2;
+	gpio_uart.Mode = GPIO_MODE_AF_PP;
+	gpio_uart.Pull = GPIO_PULLUP;
+	gpio_uart.Speed = GPIO_SPEED_FREQ_LOW;
+	gpio_uart.Alternate = GPIO_AF7_USART2;
+	HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+	gpio_uart.Pin = GPIO_PIN_3;			//3번핀은 TX로 설정하기 위함 나머지 모두 같음 AF7마저도
+	HAL_GPIO_Init(GPIOA, &gpio_uart);	//그리고 설정완료
+
+
+	//3. IRQ 활설화 및 우선순위 설정(NVIC 설정)
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+
 }
